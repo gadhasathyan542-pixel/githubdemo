d0547766875e7c88ef389e14326e93a50507833c
Gadha Sathyan <gadhasathyan542@gmail.com>
2026-05-20 17:52:54 +0530

    Update mycode.py

--- mycode.py
+++ mycode.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-print("	My first line")
-print("My second line")
-print("My latest third line")
-=======
+
 print("	My First Line")
 print("My Second Line")
 print("My most most latest Third Line")
->>>>>>> week1-client-changes
+print("test for pull")
